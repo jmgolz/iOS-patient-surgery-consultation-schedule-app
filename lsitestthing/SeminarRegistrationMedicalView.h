0eab8808f812ae0a9aa79c4b3bcd4721f3154f77
James Golz <jgolz@laserspineinstitute.com>
2015-10-07 11:11:02 -0400

    Add rest of outlets and actions to medical registration form view subclass, wire up pickers to data sources.

--- lsitestthing/SeminarRegistrationMedicalView.h
+++ lsitestthing/SeminarRegistrationMedicalView.h
@@ -10,4 +10,32 @@
 
 @interface SeminarRegistrationMedicalView : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
 
+//Picker outlets
+@property (weak, nonatomic) IBOutlet UIPickerView *insuranceCompanyPicker;
+@property (weak, nonatomic) IBOutlet UIPickerView *howDidYouHearAboutUsPicker;
+
+//Buttons - form inputs
+@property (weak, nonatomic) IBOutlet UISegmentedControl *insuranceTypeSegmentedButton;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *patientIsBringingMriSegmentedButton;
+@property (weak, nonatomic) IBOutlet UIStepper *howManyInPatientPartyStepperButton;
+
+//Buttons - form actions
+@property (weak, nonatomic) IBOutlet UIButton *saveFormDataButton;
+@property (weak, nonatomic) IBOutlet UIButton *clearFormButton;
+@property (weak, nonatomic) IBOutlet UIButton *formCancelButton;
+
+//Form action control interactions
+- (IBAction)saveDataButtonPressed:(id)sender;
+- (IBAction)clearFormButtonPressed:(id)sender;
+- (IBAction)cancelFormButtonPressed:(id)sender;
+
+//Form input control interactions
+- (IBAction)insuranceTypeSegmentedButtonPressed:(id)sender;
+- (IBAction)patientIsBringingMriSegmentedButtonPressed:(id)sender;
+- (IBAction)howManyInPatientPartyStepperButton:(id)sender;
+
+//Data sources
+@property NSArray *insuranceCompanyDataSource;
+@property NSArray *howDidYouHearAboutUsDataSource;
+
 @end
--- lsitestthing/SeminarRegistrationMedicalView.m
+++ lsitestthing/SeminarRegistrationMedicalView.m
@@ -14,15 +14,44 @@
 
 @implementation SeminarRegistrationMedicalView
 
+-(void)viewWillAppear:(BOOL)animated{
+    self.howDidYouHearAboutUsDataSource = [[NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"applicationReusableData" ofType:@"plist"]] objectAtIndex:5];
+    self.insuranceCompanyDataSource = [[NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"applicationReusableData" ofType:@"plist"]] objectAtIndex:3];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-
+    self.insuranceCompanyPicker.delegate = self;
+    self.insuranceCompanyPicker.dataSource = self;
+    
+    self.howDidYouHearAboutUsPicker.delegate = self;
+    self.howDidYouHearAboutUsPicker.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if(pickerView.tag == 0){
+        return [self.insuranceCompanyDataSource objectAtIndex:row];
+    } else {
+        return [self.howDidYouHearAboutUsDataSource objectAtIndex:row];
+    }
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    if(pickerView.tag == 0){
+        return self.insuranceCompanyDataSource.count;
+    } else  {
+        return self.howDidYouHearAboutUsDataSource.count;
+    }
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
 }
 
 /*
@@ -35,4 +64,20 @@
 }
 */
 
+- (IBAction)saveDataButtonPressed:(id)sender {
+}
+
+- (IBAction)clearFormButtonPressed:(id)sender {
+}
+
+- (IBAction)cancelFormButtonPressed:(id)sender {
+}
+- (IBAction)insuranceTypeSegmentedButtonPressed:(id)sender {
+}
+
+- (IBAction)patientIsBringingMriSegmentedButtonPressed:(id)sender {
+}
+
+- (IBAction)howManyInPatientPartyStepperButton:(id)sender {
+}
 @end
